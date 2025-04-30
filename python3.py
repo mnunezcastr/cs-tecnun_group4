48f7330a9746ca788fe67075ac9972157f6ad5d0
mnunezcastr <mnunezcastr@alumni.unav.es>
2025-04-30 10:06:29 +0200

    Create python3.py
    Initial connect

---
+++ python3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 30 10:02:25 2025
+
+@author: mnunezcastr
+"""
+
+import serial # library used to communicate with serial port
+import re # library used to extract data from string
+import tkinter as tk
+import sys
