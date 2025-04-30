27719484691c88fa4c4b8057e2ae5729e29f3d59
mnunezcastr <mnunezcastr@alumni.unav.es>
2025-04-30 11:49:09 +0200

    Update periodic_message_experiment_4_OOP.py
    serial port and file writing

--- periodic_message_experiment_4_OOP.py
+++ periodic_message_experiment_4_OOP.py
@@ -13,9 +13,12 @@ import sys
 class WeatherStation(tk.Tk):
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
-    
+
     def __init__(self, **kwargs):
         super().__init__()
+        self.SERIAL_PORT_DEFAULT = 'COM3'
+        self.SERIAL_BAUDRATE_DEFAULT = 9600
+        self.FILE_NAME_DEFAULT = './data.txt'
         self.geometry("500x200")
         self.title('Weather Station')
         self.resizable(False, False)
@@ -27,6 +30,27 @@ class WeatherStation(tk.Tk):
         self.lbl_hum.place(x=100, y=20)
         self.lbl_pres = tk.Label(master=self, text="Initial pres", font=50)
         self.lbl_pres.place(x=330, y=20)
+        if 'serial_port' in kwargs:
+            self.serial_port = kwargs['serial_port']
+        else:
+            self.serial_port = self.SERIAL_PORT_DEFAULT
+        if 'serial_baudrate' in kwargs:
+            self.serial_baudrate = kwargs['serial_baudrate']
+        else:
+            self.serial_baudrate = self.SERIAL_BAUDRATE_DEFAULT
+        if 'file_name' in kwargs:
+            self.file_name = kwargs['file_name']
+        else:
+            self.file_name = self.FILE_NAME_DEFAULT
+        self.ser = serial.Serial() # create a serial instance
+        self.ser.port = self.serial_port
+        self.ser.baudrate=self.serial_baudrate
+        try:
+            data_file=open("archivo.csv","w")
+            
+        except IOError as e:
+            print(f"Error al abrir el archivo: {e}")
+            sys.exit()
         
     def close_application(self):
         self.destroy()
@@ -72,7 +96,7 @@ class WeatherStation(tk.Tk):
     def get_weather(self) :
         global data_file
         print("get_weather() working...")
-        #message = self.ser.readline() # read one line (until EOL) from the serial port
+        message = self.ser.readline() # read one line (until EOL) from the serial port
         message = b'<temp=4.2,hum=42,pres=1042> ' #b stands for binary data
         print(message)
         temp=self.extract_temp(message)
@@ -85,8 +109,8 @@ class WeatherStation(tk.Tk):
         print(temp)
         print(hum)
         print(pres)
-        #if temp:
-            #data_file.write(f'{temp}; {pres}; {hum}\n')
+        if temp:
+            data_file.write(f'{temp}; {pres}; {hum}\n')
         if temp:
            self.lbl_temp["text"]= temp + " ºC"
            self.lbl_hum["text"]=hum + " %"
@@ -97,6 +121,6 @@ class WeatherStation(tk.Tk):
         self.mainloop()
         
     
-App = WeatherStation();
+App = WeatherStation(serial_port='COM6');
 App.run()
 
